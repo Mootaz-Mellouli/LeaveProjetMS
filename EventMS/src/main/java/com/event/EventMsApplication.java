5ca5c3d5fc97ea4e9b758690b9630da2d4846f0f
Mootaz Mellouli <mootazmellouli@hotmail.com>
2024-03-20 16:31:28 +0100

    fix event MS

--- EventMS/src/main/java/com/event/EventMsApplication.java
+++ EventMS/src/main/java/com/event/EventMsApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EventMsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EventMsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EventMsApplication.class, args);
+    }
 
 }
--- EventMS/src/test/java/com/event/EventMsApplicationTests.java
+++ EventMS/src/test/java/com/event/EventMsApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class EventMsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
