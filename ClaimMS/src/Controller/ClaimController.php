2989a04a016e8c2b18b67cc30f41f6e4b8dc2171
Mootaz Mellouli <mootazmellouli@hotmail.com>
2024-03-21 17:44:49 +0100

    api-platform + Controller + Debugger + CRUD

---
+++ ClaimMS/src/Controller/ClaimController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ClaimController extends AbstractController
+{
+    #[Route('/api/claim', name: 'book', methods: ['GET'])]
+    public function index(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/ClaimController.php',
+        ]);
+    }
+}
---
+++ ClaimMS/src/Entity/Claim.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Entity;
+
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\ClaimRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ClaimRepository::class)]
+#[ApiResource]
+class Claim
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $Description = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $ClaimDate = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?bool $FeedBackEmployee = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->Description;
+    }
+
+    public function setDescription(?string $Description): static
+    {
+        $this->Description = $Description;
+
+        return $this;
+    }
+
+    public function getClaimDate(): ?\DateTimeInterface
+    {
+        return $this->ClaimDate;
+    }
+
+    public function setClaimDate(\DateTimeInterface $ClaimDate): static
+    {
+        $this->ClaimDate = $ClaimDate;
+
+        return $this;
+    }
+
+    public function isFeedBackEmployee(): ?bool
+    {
+        return $this->FeedBackEmployee;
+    }
+
+    public function setFeedBackEmployee(?bool $FeedBackEmployee): static
+    {
+        $this->FeedBackEmployee = $FeedBackEmployee;
+
+        return $this;
+    }
+}
