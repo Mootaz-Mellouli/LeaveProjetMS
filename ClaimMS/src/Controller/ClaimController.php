f5388c5a8ba0a63144b931009d5a72e5f529d49c
Mootaz Mellouli <mootazmellouli@hotmail.com>
2024-03-22 16:06:46 +0100

    Eureka on Symfony MS

--- ClaimMS/src/Controller/ClaimController.php
+++ ClaimMS/src/Controller/ClaimController.php
@@ -2,15 +2,28 @@
 
 namespace App\Controller;
 
+use Eureka\EurekaClient;
+use Eureka\Exceptions\RegisterFailureException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ClaimController extends AbstractController
 {
-    #[Route('/api/claim', name: 'book', methods: ['GET'])]
+    /**
+     * @throws RegisterFailureException
+     */
+    #[Route('/claim', name: 'book', methods: ['GET'])]
     public function index(): JsonResponse
     {
+        $client = new EurekaClient([
+            'eurekaDefaultUrl' => 'http://localhost:8761/eureka',
+            'hostName' => 'localhost',
+            'appName' => 'ClaimMS',
+            'ip' => '127.0.0.1',
+            'port' => ['8000', true]
+        ]);
+        $client->register();
         return $this->json([
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/ClaimController.php',
