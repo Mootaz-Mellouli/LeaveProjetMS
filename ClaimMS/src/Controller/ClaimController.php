e26637e53e0d90be6f381127e47eb2873e322107
Mootaz Mellouli <mootazmellouli@hotmail.com>
2024-04-12 13:39:00 +0100

    Update ClaimController.php

--- ClaimMS/src/Controller/ClaimController.php
+++ ClaimMS/src/Controller/ClaimController.php
@@ -7,9 +7,15 @@ use Eureka\Exceptions\RegisterFailureException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class ClaimController extends AbstractController
 {
+    public function __construct(
+        private HttpClientInterface $client,
+    ) {
+    }
+
     /**
      * @throws RegisterFailureException
      */
@@ -17,8 +23,8 @@ class ClaimController extends AbstractController
     public function index(): JsonResponse
     {
         $client = new EurekaClient([
-            'eurekaDefaultUrl' => 'http://eureka-server:8761/eureka',
-            'hostName' => 'eureka-server',
+            'eurekaDefaultUrl' => 'http://mootaz:mootaz@localhost:8761/eureka/',
+            'hostName' => 'localhost',
             'appName' => 'ClaimMS',
             'ip' => '127.0.0.1',
             'port' => ['8000', true]
@@ -32,4 +38,31 @@ class ClaimController extends AbstractController
             'path' => 'src/Controller/ClaimController.php',
         ]);
     }
+
+    public function getClaimsNotArchivedByUser(): array
+    {
+        /*$this->client = $client->withOptions(
+            (new HttpOptions())
+                ->setBaseUri('https://...')
+                ->setHeaders(['header-name' => 'header-value'])
+                ->toArray()
+        );*/
+        $response = $this->client->request(
+            'GET',
+            'https://api.github.com/repos/symfony/symfony-docs'
+        );
+
+        $statusCode = $response->getStatusCode();
+        // $statusCode = 200
+        $contentType = $response->getHeaders()['content-type'][0];
+        // $contentType = 'application/json'
+        $content = $response->getContent();
+        // $content = '{"id":521583, "name":"symfony-docs", ...}'
+        $content = $response->toArray();
+        // $content = ['id' => 521583, 'name' => 'symfony-docs', ...]
+
+        return $content;
+    }
+
+
 }
